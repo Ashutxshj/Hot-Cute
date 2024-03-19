1ba733c7b0e0fb33eccc94e6116e2fd4b5dc8ef1
Ashutxshj <ashutosh06066@gmail.com>
2024-03-19 19:09:35 +0530

    M

---
+++ App.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+# Define your CNN model
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(image_height, image_width, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(2, activation='softmax')  # 2 classes: hot and cute
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(train_images, train_labels, epochs=10, validation_data=(validation_images, validation_labels))
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('Test accuracy:', test_acc)
+
+# Save the model for deployment
+model.save('hot_cute_classifier.h5')
